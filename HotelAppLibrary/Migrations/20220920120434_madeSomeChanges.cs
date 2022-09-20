dbe7c18c9c0a29c646ae57fdfce68d3f389bba9f
kenan <kenan.olmezoglu@gmail.com>
2022-09-20 14:05:27 +0200

    add migration

---
+++ HotelAppLibrary/Migrations/20220920120434_madeSomeChanges.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelAppLibrary.Migrations
+{
+    public partial class madeSomeChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                column: "ConcurrencyStamp",
+                value: "d8451bf2-3f30-4840-955c-5e3e570378f0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "318752bf-9593-4fac-94d1-5a64a7adc8b0", "AQAAAAEAACcQAAAAEDc8oAKZd+O0gfTxg4P4OG9Z4kYBQnv1WDbSy4VQGjlHJIFOgTJaPVYqVp5TlT5y3g==" });
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 50m);
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 75m);
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 105m);
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 160m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                column: "ConcurrencyStamp",
+                value: "9f6414b7-c359-4db4-94cb-eeeda50df1ec");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "9553d19c-edaa-4867-a9be-d1060dbcf21d", "AQAAAAEAACcQAAAAEA9yvFBvwwHq91005/91Y7acojnfAZJ1E4FO5o/RSj6WyePfWf40FYprVmJGQtG6Vg==" });
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 0m);
+        }
+    }
+}
